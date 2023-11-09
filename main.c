b32292ad366d0478651842bea18491b414a6c935
yeonwoolee1 <144992849+yeonwoolee1@users.noreply.github.com>
2023-11-09 15:57:32 +0900

    실습6

--- main.c
+++ main.c
@@ -5,18 +5,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	FILE *fp;
-	char str[100];
-	int i;
-	fp=fopen("sample.txt","w");
+	FILE *fp; 
+	char c; 
+	fp = fopen("sample.txt", "r"); 
+	if (fp == NULL) {
+		printf("failed to open\n");
+		return 0; 
+	}
 	
-	for(i=0; i<3;i++){
-	printf("input a word:");
-	scanf("%s",str);
-	fprintf(fp, "%s\n",str);
-}
+	while ((c=fgetc(fp)) != EOF) {
+		putchar(c);
+	}
 	fclose(fp);
-
-		
+	
 	return 0;
 }
